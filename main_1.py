f5d1fa5d87e0c0c102c80f6dfacef993217c2d6e
Shivam-2319 <shivam_b@bt.iitr.ac.in>
2024-10-09 03:23:51 +0530

    completely python code

---
+++ main_1.py
@@ -0,0 +1,40 @@
+import gzip
+import numpy as np
+
+def normalize_mid(ctcf_start, ctcf_stop):
+    ctcf_mid = (ctcf_start + ctcf_stop) / 2
+    return int(ctcf_mid)
+
+def matrix_formation(start, stop, mid):
+    relative_start = start - mid
+    relative_stop = stop - mid 
+    matrix_row = np.zeros(1001, dtype=int)  
+    
+    for i in range(relative_start, relative_stop + 1):
+        if -500 <= i <= 500:
+            matrix_row[i + 500] = 1  
+    return matrix_row
+    
+all_matrix = []
+
+with gzip.open("mapped.bed.gz", 'rt') as f:
+    for line in f:
+        line = line.strip()
+        new_list = line.split("\t")
+        ctcf_start = int(new_list[2])
+        ctcf_stop = int(new_list[3])
+        fragment_start = int(new_list[8])
+        fragment_stop = int(new_list[9])
+        len_fragment = fragment_stop - fragment_start
+        mid = normalize_mid(ctcf_start, ctcf_stop)
+        matrix_row = matrix_formation(fragment_start, fragment_stop, mid)
+        all_matrix.append((len_fragment, matrix_row))
+
+max_length = len(all_matrix)
+final_matrix = np.zeros((max_length, 1002), dtype=int)  
+
+for idx, (length, matrix) in enumerate(all_matrix):
+    final_matrix[idx, 0] = length  # First column is the length
+    final_matrix[idx, 1:] = matrix  # Remaining columns are the matrix row
+
+print(final_matrix)
