d527dc33c625cd2560753af666b8c483948a8b27
Shivam-2319 <shivam_b@bt.iitr.ac.in>
2024-10-09 12:12:49 +0530

    shell+python

--- main_1.py
+++ main_1.py
@@ -1,5 +1,6 @@
 import gzip
 import numpy as np
+import sys
 
 def normalize_mid(ctcf_start, ctcf_stop):
     ctcf_mid = (ctcf_start + ctcf_stop) / 2
@@ -8,17 +9,15 @@ def normalize_mid(ctcf_start, ctcf_stop):
 def matrix_formation(start, stop, mid):
     relative_start = start - mid
     relative_stop = stop - mid 
-    matrix_row = np.zeros(1001, dtype=int)  
+    matrix_row = np.zeros(1001, dtype=int) 
     
     for i in range(relative_start, relative_stop + 1):
         if -500 <= i <= 500:
             matrix_row[i + 500] = 1  
     return matrix_row
-    
-all_matrix = []
 
-with gzip.open("mapped.bed.gz", 'rt') as f:
-    for line in f:
+matrix_dict = {}
+for line in sys.stdin:
         line = line.strip()
         new_list = line.split("\t")
         ctcf_start = int(new_list[2])
@@ -26,15 +25,25 @@ with gzip.open("mapped.bed.gz", 'rt') as f:
         fragment_start = int(new_list[8])
         fragment_stop = int(new_list[9])
         len_fragment = fragment_stop - fragment_start
+        
+        
         mid = normalize_mid(ctcf_start, ctcf_stop)
+        
+        
         matrix_row = matrix_formation(fragment_start, fragment_stop, mid)
-        all_matrix.append((len_fragment, matrix_row))
+        
+        if len_fragment in matrix_dict:
+            matrix_dict[len_fragment] += matrix_row
+        else:
+            matrix_dict[len_fragment] = matrix_row
 
-max_length = len(all_matrix)
-final_matrix = np.zeros((max_length, 1002), dtype=int)  
+sorted_lengths = sorted(matrix_dict.keys())
+max_length = len(sorted_lengths)
 
-for idx, (length, matrix) in enumerate(all_matrix):
-    final_matrix[idx, 0] = length  # First column is the length
-    final_matrix[idx, 1:] = matrix  # Remaining columns are the matrix row
+final_matrix = np.zeros((max_length, 1002), dtype=int) 
 
-print(final_matrix)
+for idx, length in enumerate(sorted_lengths):
+    final_matrix[idx, 0] = length 
+    final_matrix[idx, 1:] = matrix_dict[length]  
+
+np.savetxt("final_matrix.txt", final_matrix, fmt="%d", delimiter="\t")
