94768dddd6184d26d8c3eea73fec7d01cd041d60
Shivam Bsiht <shivam_b@bt.iitr.ac.in>
2024-10-17 18:43:30 +0530

    Update range_identfier.py

--- range_identfier.py
+++ range_identfier.py
@@ -1,18 +1,13 @@
-import sys
 len_fragment = list()
 range_relative_dist = list()
-for line in sys.stdin:
-	line = line.strip()
-	new_list = line.split(" ")
-	range_relative_dist.append(new_list[0])
-	len_fragment.append(new_list[1])
+with open("final_matrix.txt") as file:
+    for line in  file:
+    	new_list = line.split(" ")
+    	range_relative_dist.append(int(new_list[0]))
+    	len_fragment.append(int(new_list[1]))
 len_fragment.sort()
 range_relative_dist.sort()
-print(len_fragment)
-"""
 print("longest fragment:",len_fragment[-1])
 print("shortest fragment:",len_fragment[0])
 print("lowest_range:",range_relative_dist[0])
 print("highest_range:",range_relative_dist[-1])
-"""
-
