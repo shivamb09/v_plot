32c830c2e7de59650540eff9ffd887bac8c2e24d
Shivam Bsiht <shivam_b@bt.iitr.ac.in>
2024-10-17 19:49:24 +0530

    Update v_plt_long_format.py

--- v_plt_long_format.py
+++ v_plt_long_format.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 
 values = defaultdict(int)
 
-def interval(start, stop):
+def mid_point(start, stop):
     mid = (start + stop) / 2
     return int(mid)
 
@@ -18,8 +18,8 @@ for line in sys.stdin:
         len_fragment = int(new_list[11])
         
         
-        mid_ctcf = interval(ctcf_start, ctcf_stop)
-        mid_fragment = interval(fragment_start,fragment_stop)
+        mid_ctcf = mid_point(ctcf_start, ctcf_stop)
+        mid_fragment = mid_point(fragment_start,fragment_stop)
         relative_distance = round(mid_fragment - mid_ctcf)
         
         
@@ -30,4 +30,4 @@ def matrix_formation(dict_1,output_file):
         for (relative_distance,len_fragment),count in dict_1.items():
             file.write(f"{relative_distance} {len_fragment} {count} \n")
 
-matrix_formation(values,"final_matrix.txt")
+matrix_formation(values,"final_matrix_long.txt")
